28ece4f5d8a58946a702b9eaf909eca4d57b88c8
Sigsdyne <112873714+Sigsdyne@users.noreply.github.com>
2025-10-02 16:14:55 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+let deButton = document.querySelector("header nav button");
+
+let hetMenu = document.querySelector("header nav");
+
+deButton.addEventListener("click", toggleButton);
+
+function toggleButton(){
+ hetMenu.classList.toggle("is-open");
+}
